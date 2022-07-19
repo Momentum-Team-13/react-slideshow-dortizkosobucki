dd434e966deb5b303e6829d8e35a0f6b767627b3
daniela <d.ortizkosobucki@gmail.com>
2022-07-19 10:57:30 -0400

    disabled button working on 1st slide, still need to format image, disable other buttons on last slides

--- src/slideshow.js
+++ src/slideshow.js
@@ -5,18 +5,24 @@ function SlideShow(props) {
     const [currentFilm, setCurrentFilm] = useState(films[0])
     const [currentFilmIndex, setCurrentFilmIndex] = useState(0)
     useEffect(() => {
+        if (currentFilmIndex == 0) {
+            document.getElementById("Go Back").disabled = true;
+            document.getElementById("Start Over").disabled = true;
+        }
         setCurrentFilm(films[currentFilmIndex])
     }, [films, currentFilmIndex])
+
     return (
         <div class="movie-card">
+            <h2>{currentFilm.original_title}</h2>
             <h2>{currentFilm.title}</h2>
-            <p>{currentFilm.original_title}</p>
             <p>{currentFilm.release_date}</p>
+            <img src={currentFilm.image}></img>
             <p>{currentFilm.description}</p>
             <p>{currentFilmIndex}</p>
-            <button onClick={() => { setCurrentFilmIndex(currentFilmIndex - 1) }}>Go Back</button>
-            <button onClick={() => { setCurrentFilmIndex(0) }}>Start Over</button>
-            <button onClick={() => { setCurrentFilmIndex(currentFilmIndex + 1) }}>Go Forward</button>
+            <button id="Go Back" onClick={() => { setCurrentFilmIndex(currentFilmIndex - 1) }}>Go Back</button>
+            <button id="Start Over" onClick={() => { setCurrentFilmIndex(0) }}>Start Over</button>
+            <button id="Go Forward" onClick={() => { setCurrentFilmIndex(currentFilmIndex + 1) }}>Go Forward</button>
         </div>
     )
 }
